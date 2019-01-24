a2e92e5e9f07d2bb274210ba28c57f5b0b785bae
Lukasz Wojciechowski - Codete <lukasz.wojciechowski@codete.com>
2019-01-24 12:52:17 +0100

    Added MAC address to the output

--- start_nginx.sh
+++ start_nginx.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 hostname=`hostname -f`
+macaddress=`ip link show eth0 | grep ether | awk '{print $2}'`
 sed -i "s/XXX/${hostname}/" /usr/share/nginx/html/index.html
+sed -i "s/YYY/${macaddress}/" /var/www/index.html
 /usr/sbin/nginx -g "daemon off;"
