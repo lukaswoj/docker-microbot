3ed38bbc2f9b9ed27c99daf4f58f2871fec74822
Lukasz Wojciechowski - Codete <lukasz.wojciechowski@codete.com>
2019-01-24 12:59:42 +0100

    a fix

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-hostname=`hostname -f`
-macaddress=`ip link show eth0 | grep ether | awk '{print $2}'`
-sed -i "s/XXX/${hostname}/" /var/www/index.html
-sed -i "s/YYY/${macaddress}/" /var/www/index.html
-/bin/asmttpd /var/www
--- start_nginx.sh
+++ start_nginx.sh
@@ -2,5 +2,5 @@
 hostname=`hostname -f`
 macaddress=`ip link show eth0 | grep ether | awk '{print $2}'`
 sed -i "s/XXX/${hostname}/" /usr/share/nginx/html/index.html
-sed -i "s/YYY/${macaddress}/" /var/www/index.html
+sed -i "s/YYY/${macaddress}/" /usr/share/nginx/html/index.html
 /usr/sbin/nginx -g "daemon off;"
